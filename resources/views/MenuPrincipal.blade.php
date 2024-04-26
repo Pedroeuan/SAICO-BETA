651b356db6f2d66f2dd613da3f46cf7f2e6394b0
Pdro Eduardo Euan Graniel <pedro.euan@aicosc.com>
2024-04-26 16:55:31 -0600

    SEGUNDO COMMIT

--- resources/views/MenuPrincipal.blade.php
+++ resources/views/MenuPrincipal.blade.php
@@ -11,17 +11,142 @@
 <br>
 <br>
 <div class="all">
-<div class="lefter">
-  <div class="text">  
-  <ul class="nav flex-column">
+          <div class="lefter">
+                  <div class="text">
+                              <ul class="nav flex-column">
                                     <li class="nav-item dropdown">  
                                             <a class="nav-link dropdown-toggle " href="" data-toggle="dropdown" aria-expanded="false">
-                                                <i class="fas fa-chalkboard-teacher"></i>
+                                                <!--<i class="fas fa-chalkboard-teacher"></i>-->
                                                     ADMINISTRATIVO
                                             </a>
                                             <ul class="dropdown-menu border-0 shadow">
                                                     <li>
                                                         <a class="dropdown-item " href="#">
+                                                            <i class="fas fa-tools "></i>
+                                                                1
+                                                        </a>
+                                                    </li>
+                                                    <li>
+                                                        <a class="dropdown-item " href="#">
+                                                            <i class="fas fa-vials "></i>
+                                                                2
+                                                        </a>
+                                                    </li>
+
+                                                    <li>
+                                                        <a class="dropdown-item " href="#">
+                                                            <i class="fas fa-suitcase "></i>
+                                                               3
+                                                        </a>
+                                                    </li>
+                                                    <li>
+                                                        <a class="dropdown-item " href="#">
+                                                                        <i class="fas fa-file-alt "></i> 
+                                                            4
+                                                        </a>
+                                                    </li>
+                                            </ul>
+                                    </li>
+                                </ul>
+          </div>
+  </div>
+
+  <div class="left">
+      <div class="text">
+      <ul class="nav flex-column">
+                                    <li class="nav-item dropdown">  
+                                            <a class="nav-link dropdown-toggle " href="" data-toggle="dropdown" aria-expanded="false">
+                                                <!--<i class="fas fa-chalkboard-teacher"></i>-->
+                                                    OPERATIVO
+                                            </a>
+                                            <ul class="dropdown-menu border-0 shadow">
+                                                    <li>
+                                                        <a class="dropdown-item " href="#">
+                                                            <i class="fas fa-tools "></i>
+                                                                1
+                                                        </a>
+                                                    </li>
+                                                    <li>
+                                                        <a class="dropdown-item " href="#">
+                                                            <i class="fas fa-vials "></i>
+                                                                2
+                                                        </a>
+                                                    </li>
+
+                                                    <li>
+                                                        <a class="dropdown-item " href="#">
+                                                            <i class="fas fa-suitcase "></i>
+                                                               3
+                                                        </a>
+                                                    </li>
+                                                    <li>
+                                                        <a class="dropdown-item " href="#">
+                                                                        <i class="fas fa-file-alt "></i> 
+                                                            4
+                                                        </a>
+                                                    </li>
+                                            </ul>
+                                    </li>
+                                </ul>
+      </div>
+
+</div>
+
+<div class="center"><!-- Verificar el center-->
+<div class="explainer">
+  <img src="vendor/adminlte\dist/img/Logos/Logo_AICO.png" alt="Imagen de ejemplo">
+  <span></span>
+</div>
+  <div class="text">
+        <ul class="nav flex-column">
+                                    <li class="nav-item dropdown">  
+                                            <a class="nav-link dropdown-toggle " href="" data-toggle="dropdown" aria-expanded="false">
+                                                <!--<i class="fas fa-chalkboard-teacher"></i>-->
+                                                    CLIENTES
+                                            </a>
+                                            <ul class="dropdown-menu border-0 shadow">
+                                                    <li>
+                                                        <a class="dropdown-item " href="#">
+                                                            <i class="fas fa-tools "></i>
+                                                                1
+                                                        </a>
+                                                    </li>
+                                                    <li>
+                                                        <a class="dropdown-item " href="#">
+                                                            <i class="fas fa-vials "></i>
+                                                                2
+                                                        </a>
+                                                    </li>
+
+                                                    <li>
+                                                        <a class="dropdown-item " href="#">
+                                                            <i class="fas fa-suitcase "></i>
+                                                               3
+                                                        </a>
+                                                    </li>
+                                                    <li>
+                                                        <a class="dropdown-item " href="#">
+                                                                        <i class="fas fa-file-alt "></i> 
+                                                            4
+                                                        </a>
+                                                    </li>
+                                            </ul>
+                                    </li>
+                                </ul>
+      </div>
+    </div>
+
+<div class="right">
+  <div class="text">
+  <ul class="nav flex-column">
+                                    <li class="nav-item dropdown">  
+                                            <a class="nav-link dropdown-toggle " href="" data-toggle="dropdown" aria-expanded="false">
+                                                <!--<i class="fas fa-chalkboard-teacher"></i>-->
+                                                 EQUIPOS Y <br> CONSUMIBLES
+                                            </a>
+                                            <ul class="dropdown-menu border-0 shadow">
+                                                    <li>
+                                                        <a class="dropdown-item " href="/Equipos">
                                                             <i class="fas fa-tools "></i>
                                                                 Equipos
                                                         </a>
@@ -36,37 +161,61 @@
                                                     <li>
                                                         <a class="dropdown-item " href="#">
                                                             <i class="fas fa-suitcase "></i>
-                                                                Kits 
+                                                               Kits
                                                         </a>
                                                     </li>
                                                     <li>
                                                         <a class="dropdown-item " href="#">
                                                                         <i class="fas fa-file-alt "></i> 
-                                                            Solicitud E y C
+                                                            Solicitud Equipos y Consumibles
                                                         </a>
                                                     </li>
                                             </ul>
                                     </li>
                                 </ul>
-
-</div>
-</div>
-<div class="left">
-  <div class="text">Web Design</div>
-</div>
-
-<div class="center"><!-- Verificar el center-->
-<div class="explainer">
-  <img src="vendor/adminlte\dist/img/Logos/Logo_AICO.png" alt="Imagen de ejemplo">
-  <span></span>
-</div>
-
-  <div class="text"></div>
   </div>
-<div class="right">
-  <div class="text">Backend Development</div>
 </div>
+
 <div class="righter">
-  <div class="text">SEO</div>
+        <div class="text">
+                <ul class="nav flex-column">
+                                    <li class="nav-item dropdown">  
+                                            <a class="nav-link dropdown-toggle " href="" data-toggle="dropdown" aria-expanded="false">
+                                                <!--<i class="fas fa-chalkboard-teacher"></i>-->
+                                                    SUPERVISOR
+                                            </a>
+                                            
+                                            <ul class="dropdown-menu border-0 shadow">
+                                                    <li>
+                                                        <a class="dropdown-item" href="#">
+                                                            <i class="fas fa-tools "></i>
+                                                                1
+                                                        </a>
+                                                    </li>
+                                                    <li>
+                                                        <a class="dropdown-item " href="#">
+                                                            <i class="fas fa-vials "></i>
+                                                                2
+                                                        </a>
+                                                    </li>
+
+                                                    <li>
+                                                        <a class="dropdown-item " href="#">
+                                                            <i class="fas fa-suitcase "></i>
+                                                               3
+                                                        </a>
+                                                    </li>
+                                                    <li>
+                                                        <a class="dropdown-item " href="#">
+                                                                        <i class="fas fa-file-alt "></i> 
+                                                            4
+                                                        </a>
+                                                    </li>
+                                            </ul>
+                                    </li>
+                                </ul>  
+
+  </div>
 </div>
+
 </div>
