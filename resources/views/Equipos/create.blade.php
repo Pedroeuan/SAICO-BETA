e59d2800751d4242de6a945cfe74c95a198e05a0
yacziryGomez <yacziry.gomez@aicosc.com>
2024-06-26 16:20:24 -0600

    update-yacziry

--- resources/views/Equipos/create.blade.php
+++ resources/views/Equipos/create.blade.php
@@ -411,7 +411,7 @@
                         </div>
                         <!-- BLOCKS -->
                         <div class="tab-pane" id="tab_4">
-                        <form id="blocksForm" action="{{route('general_eyc.storeBlocks')}}" method="post" enctype="multipart/form-data">
+                            <form id="blocksForm" action="{{route('general_eyc.storeBlocks')}}" method="post" enctype="multipart/form-data">
                             @csrf
                                 <div class="row">
                                     <div class="col-sm-4">
@@ -660,39 +660,34 @@
                                         <div class="float-right">
                                             <button type="submit" class="btn btn-info bg-primary">Finalizar</button>
                                         </div>
-
                                         <div class="float-left">
                                             <button type="button" class="btn btn-info bg-success" id="guardarContinuarHerramientas">Guardar y continuar</button>
                                         </div>
                                     </div>
-
                                 </div>
                             </form>
                         </div>
                         <!-- Agrega más paneles de tabs según sea necesario -->
                         <!--KITS -->
-                            <div class="tab-pane" id="tab_6">
-                                <form id="kitForm" method="post" enctype="multipart/form-data" action="{{ route('GuardarKits.agregarKits') }}">
-                                    @csrf
-                                    <div class="box">
-                                        <h5 align="center">Inventario</h5>
-
-                                        <div class="d-flex justify-content-between align-items-center mb-3">
-                                            <div class="col-sm-4">
-                                                <div class="form-group">
-                                                    <label class="col-form-label" for="inputSuccess">Nombre</label>
-                                                    <input type="text" class="form-control inputForm" name="Nombre" placeholder="Ejemplo: Kit de Liquidos">
-                                                </div>
+                        <div class="tab-pane" id="tab_6">
+                            <form id="kitForm" method="post" enctype="multipart/form-data" action="{{ route('GuardarKits.agregarKits') }}">
+                                @csrf
+                                <div class="box">
+                                    <div class="d-flex justify-content-between align-items-center mb-3">
+                                        <div class="col-sm-4">
+                                            <div class="form-group">
+                                                <label class="col-form-label" for="inputSuccess">Nombre</label>
+                                                <input type="text" class="form-control inputForm" name="Nombre" placeholder="Ejemplo: Kit de Liquidos">
                                             </div>
-
-                                            <div class="col-sm-4">
-                                                <div class="form-group">
-                                                    <label class="col-form-label" for="inputSuccess">Prueba</label>
-                                                    <input type="text" class="form-control inputForm" name="Prueba" placeholder="Ejemplo: Liquidos">
-                                                </div>
+                                        </div>
+                                        <div class="col-sm-4">
+                                            <div class="form-group">
+                                                <label class="col-form-label" for="inputSuccess">Prueba</label>
+                                                <input type="text" class="form-control inputForm" name="Prueba" placeholder="Ejemplo: Liquidos">
                                             </div>
                                         </div>
                                     </div>
+<<<<<<< Updated upstream
 
                                     <!-- Tabla de Elementos Disponibles -->
                                     <table id="tablaJs" class="table table-bordered table-striped dt-responsive tablas">
@@ -766,6 +761,84 @@
                                 </form>
                             </div><!--"class="tab-pane" id="tab_6""-->
 
+=======
+                                </div>
+                                <!-- Tabla de Elementos Disponibles -->
+                                <table id="tablaJs" class="table table-bordered table-striped dt-responsive tablas" style="width:100%">
+                                    <thead>
+                                        <tr>
+                                            <th>Nombre</th>
+                                            <th>Num. Económico</th>
+                                            <th>Marca</th>
+                                            <th>Modelo</th>
+                                            <th>NS</th>
+                                            <th>Disponibilidad</th>
+                                            <th>Fecha calibración</th>
+                                            <th>Hoja de Presentación</th>
+                                            <th>Acciones</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($generalConCertificados as $general_eyc)
+                                        <tr data-id="{{ $general_eyc->idGeneral_EyC }}">
+                                            <td>{{ $general_eyc->Nombre_E_P_BP }}</td>
+                                            <td>{{ $general_eyc->No_economico }}</td>
+                                            <td>{{ $general_eyc->Marca }}</td>
+                                            <td>{{ $general_eyc->Modelo }}</td>
+                                            <td>{{ $general_eyc->Serie }}</td>
+                                            <td>
+                                                @if($general_eyc->Disponibilidad_Estado=='DISPONIBLE')
+                                                <button type="button" class="btn btn-success"><i class="fa fa-check" aria-hidden="true"></i></button>
+                                                @elseif($general_eyc->Disponibilidad_Estado=='NO DISPONIBLE')
+                                                <button type="button" class="btn btn-warning"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i></button>
+                                                @elseif($general_eyc->Disponibilidad_Estado=='FUERA DE SERVICIO/BAJA')
+                                                <button type="button" class="btn btn-danger"><i class="fa fa-ban" aria-hidden="true"></i></button>
+                                                @elseif($general_eyc->Disponibilidad_Estado=='ESPERA DE DATO')
+                                                <button type="button" class="btn btn-info"><i class="far fa-clock" aria-hidden="true"></i></button>
+                                                @endif
+                                            </td>
+                                            <td>{{ $general_eyc->certificados ? $general_eyc->certificados->Fecha_calibracion : 'N/A' }}</td>
+                                            <td>
+                                                @if ($general_eyc->Foto != 'ESPERA DE DATO')
+                                                <a class="btn btn-primary" href="{{ asset('storage/' . $general_eyc->Foto) }}" role="button" target="_blank"><i class="fa fa-eye"></i></a>
+                                                @else
+                                                <a target="_blank" class="btn btn-secondary" role="button"><i class="fa fa-ban" aria-hidden="true"></i></a>
+                                                @endif
+                                            </td>
+                                            <td>
+                                                <button type="button" class="btn btn-success btnAgregar" data-id="{{ $general_eyc->idGeneral_EyC }}"><i class="fas fa-plus-circle" aria-hidden="true"></i></button>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                                <!-- Tabla de Elementos Seleccionados -->
+                                <table id="tablaSeleccionados" class="table table-bordered table-striped dt-responsive tablas">
+                                    <thead>
+                                        <tr>
+                                            <th>Nombre</th>
+                                            <th>Num. Económico</th>
+                                            <th>Marca</th>
+                                            <th>Ultima calibración</th>
+                                            <th>Cantidad</th>
+                                            <th>Unidad</th>
+                                            <th>Acciones</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    </tbody>
+                                </table>
+                                <div class="container">
+                                    <div class="float-right">
+                                        <button type="submit" class="btn btn-info bg-primary">Finalizar</button>
+                                    </div>
+                                    <div class="float-left">
+                                        <button type="button" class="btn btn-info bg-success" id="guardarContinuarKits">Guardar y continuar</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div><!--"class="tab-pane" id="tab_6""-->
+>>>>>>> Stashed changes
                     </div><!-- /.tab-content -->
                 </div><!-- /.card-body -->
             </div><!-- /.card -->       
