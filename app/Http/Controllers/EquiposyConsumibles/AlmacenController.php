133c622be806ba23a062ed4b1117317b8774c30e
Pdro Eduardo Euan Graniel <pedro.euan@aicosc.com>
2024-07-15 18:54:23 -0600

    update
    Solicitud- al agregar los E y C que tengan en stock de Alamcen 1, los campos no se editan, pero los otros como los  consumibles, si

--- app/Http/Controllers/EquiposyConsumibles/AlmacenController.php
+++ app/Http/Controllers/EquiposyConsumibles/AlmacenController.php
@@ -2,11 +2,40 @@
 
 namespace App\Http\Controllers\EquiposyConsumibles;
 
-use App\Models\EquiposyConsumibles\almacen;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Log;
+
+use App\Models\EquiposyConsumibles\general_eyc;
+use App\Models\EquiposyConsumibles\equipos;
+use App\Models\EquiposyConsumibles\certificados;
+use App\Models\EquiposyConsumibles\consumibles;
+use App\Models\EquiposyConsumibles\almacen;
+use App\Models\EquiposyConsumibles\Historial_Almacen;
+use App\Models\EquiposyConsumibles\accesorios;
+use App\Models\EquiposyConsumibles\block_y_probeta;
+use App\Models\EquiposyConsumibles\herramientas;
+use App\Models\EquiposyConsumibles\historial_certificado;
+use App\Models\EquiposyConsumibles\detalles_kits;
+use App\Models\EquiposyConsumibles\kits;
+
 
 class AlmacenController extends Controller
 {
+
+    public function obtenerCantidadAlmacen($id)
+    {
+        // Consulta la cantidad del elemento en la tabla 'almacen'
+        $Almacen = Almacen::find($id);
+        
+        if ($Almacen) {
+            return response()->json(['Cantidad' => $Almacen->Stock]);
+        } else {
+            return response()->json(['error' => 'Elemento no encontrado'], 404);
+        }
+    }
     /**
      * Display a listing of the resource.
      */
