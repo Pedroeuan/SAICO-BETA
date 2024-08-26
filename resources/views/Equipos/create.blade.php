12d7a670fa2f534574ca338603fae7e1533f0e0b
Pdro Eduardo Euan Graniel <pedro.euan@aicosc.com>
2024-08-26 10:39:49 -0600

    pedro
    detalle de un div de cierre hacia que visualmente se montara el apartado de kits en las demás pestañas

--- resources/views/Equipos/create.blade.php
+++ resources/views/Equipos/create.blade.php
@@ -937,9 +937,10 @@
                                                     <input type="text" class="form-control inputForm" name="Prueba" placeholder="Ejemplo: Liquidos" required>
                                                 </div>
                                             </div>
+
                                         </div>
-                                    </div>  
-                                </div>
+                                </div>  
+                                
                                 <br>
                                 <div class="alert alert-info alert-dismissible">
                                     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
