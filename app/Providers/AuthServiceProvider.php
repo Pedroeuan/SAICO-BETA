a32c406853796c278c324c20a9bed15114cf9288
Pdro Eduardo Euan Graniel <pedro.euan@aicosc.com>
2024-08-29 15:12:44 -0600

    Pedro-update-roles
    SAICO | se asigna el rol para Administrador, Técnicos y Equipos, en el menú principal, falta definir en las rutas

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,34 +26,37 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         // Define tus políticas de acceso aquí
-        Gate::define('Superadmin-access', function ($user) { 
+        /*Gate::define('Superadmin-access', function ($user) { 
             return $user->rol === 'Super Administrador';
-        });
+        });*/
 
-        Gate::define('administrador-access', function ($user) {
-            return in_array($user->rol, ['Administrador']);
+        Gate::define('administrador-access', function ($user) { 
+            return $user->rol === 'Administrador' || $user->rol === 'Super Administrador';
         });
 
         Gate::define('cliente-access', function ($user) {
-            return $user->rol === 'Cliente';
+            return $user->rol === 'Cliente' || $user->rol === 'Super Administrador';
         });
 
         Gate::define('ventas-access', function ($user) {
-            return $user->rol === 'Ventas';
+            return $user->rol === 'Ventas' || $user->rol === 'Super Administrador';
         });
 
         Gate::define('tecnicos-access', function ($user) {
-            return $user->rol === 'Técnicos';
+            return $user->rol === 'Técnicos' || $user->rol === 'Super Administrador';
         });
 
         Gate::define('planeacion-access', function ($user) {
-            return $user->rol === 'Planeación';
+            return $user->rol === 'Planeación' || $user->rol === 'Super Administrador';
         });
 
         Gate::define('equipos-access', function ($user) {
-            return $user->rol === 'Equipos';
+            return $user->rol === 'Equipos' || $user->rol === 'Super Administrador';
         });
 
+        Gate::define('tecnicos-equipos-access', function ($user) {
+            return $user->rol === 'Equipos' || $user->rol === 'Super Administrador' || $user->rol === 'Técnicos';
+        });
         
     }
 }
