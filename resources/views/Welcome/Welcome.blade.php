440fbcf9d9411f13ef1b178bf73cca6799d2a9d1
Pedroeuan <pedro.euan@aicosc.com>
2025-01-18 12:25:24 -0600

    update-welcome-notificacion

--- resources/views/Welcome/Welcome.blade.php
+++ resources/views/Welcome/Welcome.blade.php
@@ -43,29 +43,25 @@
 @stop
 
 @section('js')
+<!-- Incluye jQuery -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<!--datatable -->
+<script src="https://cdn.datatables.net/2.0.7/js/dataTables.js"></script>
+<script src="https://cdn.datatables.net/2.0.7/js/dataTables.bootstrap5.js"></script>
+<!--<script src="https://cdn.datatables.net/2.0.8/js/jquery.dataTables.min.js"></script>-->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://cdn.datatables.net/v/bs5/jqc-1.12.4/dt-2.1.4/datatables.min.css" rel="stylesheet">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.datatables.net/v/bs5/jqc-1.12.4/dt-2.1.4/datatables.min.js"></script>
+<!-- Select2 JS -->
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<!--sweet alert -->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<script src="{{ asset('js/session-handler.js') }}"></script>
 <script>
-    // Temporizador de cuenta regresiva
-    /*const countdownElement = document.getElementById('countdown');
-    const endDate = new Date('2024-12-31T23:59:59').getTime();
-
-    function updateCountdown() {
-        const now = new Date().getTime();
-        const timeLeft = endDate - now;
-
-        if (timeLeft > 0) {
-            const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-            const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-            const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-
-            countdownElement.textContent = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-        } else {
-            countdownElement.textContent = '¡Ya está disponible!';
-        }
-    }
-
-    updateCountdown();
-    setInterval(updateCountdown, 1000);*/
+    const updateNotificationUrl = "{{ url('notificaciones/update') }}";
+    const viewAllNotificationsUrl = "{{ url('notificacion/index') }}";
 </script>
-
+<script src="{{ asset('js/notificaciones.js') }}"></script>
 @endsection
