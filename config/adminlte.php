afa7865bdd74818fd5c9d4e154549682b47bc52c
Pedroeuan <pedro.euan@aicosc.com>
2025-01-27 12:20:34 -0600

    menu sin liberar

--- config/adminlte.php
+++ config/adminlte.php
@@ -457,17 +457,7 @@ return [
                     [
                         'text' => 'Reportes',
                         'icon' => 'fab fa-wpforms',
-                        'url' => '/Menu/Servicios',
-                    ],
-                    [
-                        'text' => 'Registro de Pruebas',
-                        'icon' => 'fas fa-indent',
-                        'url' => '/Pruebas/Create',
-                    ],
-                    [
-                        'text' => 'Pruebas',
-                        'icon' => 'fas fa-table',
-                        'url' => '/index/Pruebas',
+                        'url' => '/Page_In_Construction',
                     ],
                     /* [
                         'text' => 'level_one',
@@ -523,12 +513,12 @@ return [
                     [
                         'text' => 'Registro OC',
                         'icon' => 'fas fa-chart-line',
-                        'url' => '/OC/createOC',
+                        'url' => '/Page_In_Construction',
                     ],
                     [
                         'text' => 'Ordenes de Compras',
                         'icon' => 'fas fa-clipboard-list',
-                        'url' => '/OC/indexOC',
+                        'url' => '/Page_In_Construction',
                     ],
                     /*[
                         'text' => '',
