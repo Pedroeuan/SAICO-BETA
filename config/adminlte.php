39a649e968b1041ad0226239d8782145044fab84
Pedroeuan <pedro.euan@aicosc.com>
2025-03-08 13:30:53 -0600

    update

--- config/adminlte.php
+++ config/adminlte.php
@@ -454,7 +454,7 @@ return [
                     [
                         'text' => 'Pruebas',
                         'icon' => 'fas fa-table',
-                        'url' => '/Page_In_Construction',
+                        'url' => '/index/Pruebas',
 
                     ],
                 ],
