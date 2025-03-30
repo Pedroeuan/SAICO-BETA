b8425e8cddcb29a7f47b8d856d196037679793b4
Pedroeuan <pedro.euan@aicosc.com>
2025-03-29 23:13:18 -0600

    update-fotos
    pequeño ajuste a la anchura de las fotos

--- resources/views/Reportes/ReportesFotosPDF/Reporte_FOTOS_FOR_INS_10_02_PDF.blade.php
+++ resources/views/Reportes/ReportesFotosPDF/Reporte_FOTOS_FOR_INS_10_02_PDF.blade.php
@@ -147,7 +147,7 @@
 
         .foto-container {
             padding: 0; /* Asegura que la imagen toque el borde de la celda */
-            width: 310px;  /* Fija el ancho de la celda */
+            width: 312px;  /* Fija el ancho de la celda */
             height: 170px; /* Fija la altura de la celda */
             border: 1px solid black; 
             vertical-align: middle;
@@ -156,7 +156,7 @@
         .foto-container img {
             /*object-fit: contain; /* Ajusta la imagen dentro del recuadro sin recortarla */
             object-fit: cover; /* Llenar el espacio sin distorsionar */
-            width: 310px;  /* Ajusta el ancho de la celda */
+            width: 312px;  /* Ajusta el ancho de la celda */
             height: 170px; /* Ajusta la altura de la celda */
             vertical-align: middle;
             display: flex;
