c4237b5a402d6102ff9710bdabab4fd8a4da8c8b
Pedroeuan <pedro.euan@aicosc.com>
2025-05-23 18:37:42 -0600

    udpate

--- resources/views/Reportes/ReportesPDF/Reporte_FOR_INS_02_02_PDF.blade.php
+++ resources/views/Reportes/ReportesPDF/Reporte_FOR_INS_02_02_PDF.blade.php
@@ -460,7 +460,7 @@
                                 </tr>
                             </thead>
                             <tbody>
-                            @for($i = 0; $i < 54; $i++)
+                            @for($i = 0; $i < 14; $i++)
                             <tr>
                                 <td>----</td>
                                 <td>----</td>
