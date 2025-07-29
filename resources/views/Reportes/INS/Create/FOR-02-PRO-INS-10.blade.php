c09deb474b926948d41e2b15501c99a95a2da0c9
Pedroeuan <pedro.euan@aicosc.com>
2025-07-28 18:27:02 -0600

    update-for-01-ins-12
    SAICO | Se actualiza el formato FOR-01-INS-12

--- resources/views/Reportes/INS/Create/FOR-02-PRO-INS-10.blade.php
+++ resources/views/Reportes/INS/Create/FOR-02-PRO-INS-10.blade.php
@@ -1003,7 +1003,7 @@
                     <td><input type="text" class="form-control" name="metros_lineales[${lastTitle}][]" placeholder="Metros Lineales"></td>
                     <td><input type="text" class="form-control" name="evaluacion[${lastTitle}][]" placeholder="Evaluación"></td>
                     <td><input type="text" class="form-control" name="observaciones[${lastTitle}][]" placeholder="Observaciones"></td>
-                    <td><button type="button" class="btn btn-danger btnEliminar"></button></td>
+                    <td><button type="button" class="btn btn-danger btnEliminar">   <i class="fa fa-times"  aria-hidden="true"></i></button></td>
                 </tr>`;
 
                 $('#dynamicTable tbody').append(newRow);
@@ -1121,7 +1121,7 @@
         // Guardar en localStorage al escribir
         //form.querySelectorAll('input:not([type="file"]), textarea, select').forEach(function (el) {
           //  el.addEventListener('input', function () {
-            //    localStorage.setItem('FOR-01-PRO-INS-12_' + el.name, el.value);
+            //    localStorage.setItem('FOR-02-PRO-INS-10_' + el.name, el.value);
             //});
         //});
 
