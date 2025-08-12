bb96c3289770a5e7f70f8822d87899ad83bedc9c
Pedroeuan <pedro.euan@aicosc.com>
2025-08-12 16:25:13 -0600

    update

--- app/Http/Controllers/Notificacion/NotificacionController.php
+++ app/Http/Controllers/Notificacion/NotificacionController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers\Notificacion;
 
-use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Log;
-use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
 
 use App\Models\EquiposyConsumibles\general_eyc;
 use App\Models\EquiposyConsumibles\equipos;
