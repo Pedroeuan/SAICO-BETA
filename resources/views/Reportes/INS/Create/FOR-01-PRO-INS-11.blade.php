991179e0cc078efeadac7907cca98d360a235bde
Gabriel1-core <Gabriel.Zamora@aicosc.com>
2025-08-21 18:33:28 -0600

    PRO-01-INS-11
    CREACION

---
+++ resources/views/Reportes/INS/Create/FOR-01-PRO-INS-11.blade.php
@@ -0,0 +1,57 @@
+@extends('adminlte::page')
+
+@section('title', 'FOR-01-PRO-INS-11')
+
+<style>
+        table {
+            width: 100%; /* Opcional: Para que ocupe todo el ancho disponible */
+            border-collapse: collapse; /* Elimina los espacios entre bordes */
+        }
+
+        table th, table td {
+            text-align: center; /* Centra el texto horizontalmente */
+            vertical-align: middle; /* Centra el texto verticalmente */
+            padding: 8px; /* Espaciado interno para mayor claridad */
+        }
+
+        table input {
+            text-align: center; /* Centra el texto dentro de los inputs */
+            box-sizing: border-box; /* Garantiza que los inputs respeten los bordes */
+        }
+
+        .image-preview {
+            width: 100%;
+            max-width: 200px;
+            height: 200px;
+            border: 1px solid #ddd;
+            margin-top: 10px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            overflow: hidden;
+        }
+
+        .image-preview img {
+            max-width: 100%;
+            max-height: 100%;
+            object-fit: contain; /* Cambia a 'contain' para que la imagen se ajuste dentro del contenedor sin recortarse */
+        }
+
+        .custom-btn {
+        background-color: #198754 !important; /* Verde */
+        color: white !important;
+        border: none !important;
+        border-radius: 5px !important;
+        cursor: pointer !important;
+        }
+
+        .custom-btn:hover {
+            background-color: #218838 !important; /* Verde más oscuro */
+        }
+        #my-notification .dropdown-menu {
+        max-height: 200px; /* Ajusta la altura según sea necesario */
+        overflow-y: auto;
+        }
+    </style>
+
+@endsection
