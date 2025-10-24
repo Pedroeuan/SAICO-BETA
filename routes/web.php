a306e7a631a9c00e8e563fea8f03e837db264d81
Pedroeuan <pedro.euan@aicosc.com>
2025-10-23 18:14:15 -0600

    update

--- routes/web.php
+++ routes/web.php
@@ -58,9 +58,15 @@ use App\Http\Controllers\Reporte\INS\FOR_02_PRO_INS_10Controller;
 use App\Http\Controllers\Reporte\INS\FOR_02_PRO_INS_15Controller;
 use App\Http\Controllers\Reporte\INS\FOR_03_PRO_INS_15Controller;
 
-    Route::get('/', function () {
-        return view('auth.login');
-    });
+    require __DIR__.'/auth.php';
+
+    Auth::routes();
+
+    Route::get('/register', function () {return view('auth.login');});
+
+    Auth::routes(['register' => false]);
+
+    Route::redirect('/register', '/login');
 
     Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
@@ -541,9 +547,4 @@ use App\Http\Controllers\Reporte\INS\FOR_03_PRO_INS_15Controller;
     });
 });
 
-require __DIR__.'/auth.php';
-
-Auth::routes();
-
-//Route::get('/home',[App\Http\Controller\HomeController::class,'index'])->name('home');
 
