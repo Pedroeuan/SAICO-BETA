eff5f7ee64875ecfe9eda649eda59933d81194e7
Pedroeuan <pedro.euan@aicosc.com>
2025-12-10 11:26:58 -0600

    ip

--- routes/web.php
+++ routes/web.php
@@ -65,7 +65,6 @@ use App\Http\Controllers\UsuarioController;
     Auth::routes();
 
     Route::redirect('/', '/dashboard');
-    Route::redirect('/login', '/dashboard');
     Route::redirect('/register', '/dashboard');
 
     //solicitud_AD
