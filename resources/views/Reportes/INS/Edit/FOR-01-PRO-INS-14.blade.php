235a2d35d7787b934ffc0f9547294d24c0513c5f
Pedroeuan <pedro.euan@aicosc.com>
2026-01-16 17:21:01 -0600

    update

--- resources/views/Reportes/INS/Edit/FOR-01-PRO-INS-14.blade.php
+++ resources/views/Reportes/INS/Edit/FOR-01-PRO-INS-14.blade.php
@@ -1130,7 +1130,7 @@
                     </div>
 
                     <p>
-
+//
                     <div class="alert alert alert-info alert-dismissible">
                         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
                         <h5><i class="icon fas fa-info"></i> Información</h5>
