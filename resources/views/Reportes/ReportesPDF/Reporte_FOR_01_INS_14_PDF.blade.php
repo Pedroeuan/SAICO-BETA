55ef8bc7c2e489e342c3c71b66679a9bc1a991ea
Pedroeuan <pedro.euan@aicosc.com>
2026-01-19 16:40:20 -0600

    update-pdf

--- resources/views/Reportes/ReportesPDF/Reporte_FOR_01_INS_14_PDF.blade.php
+++ resources/views/Reportes/ReportesPDF/Reporte_FOR_01_INS_14_PDF.blade.php
@@ -16,65 +16,66 @@
                     2.2cm; /* izquierdo */
                 }
 
+            @if ($totalTitulosYFilas <=15)
                 header {
                     width: 100%;
                     top: -30px; /* Ajusta para que no interfiera con el margen de la página */
                     height: auto; /* Permite crecer según el contenido */
                     text-align: center;
-                    background-color: rgb(226, 45, 45);
+                    /*background-color: rgb(226, 45, 45);*/
                     font-family: 'arial', sans-serif;
                 }
 
                 footer {
                     position: fixed;
-                    bottom: 60px;
+                    bottom: -30px;
                     left: 0;
                     right: 0;
                     height: auto;
                     text-align: center;
-                    background-color: rgb(7, 231, 18);
+                    /*background-color: rgb(7, 231, 18);*/
                     font-family: 'arial', sans-serif;
                 }
 
                 body {
-                    margin: -30px, 50; /* Ajusta el margen de la página */
+                    margin: -30px, 0; /* Ajusta el margen de la página */
                     padding-bottom: 60px; /* Para que el contenido no se monte en el footer */
                     font-family: 'arial', sans-serif;
-                    background-color: rgb(45, 78, 226);
+                    /*background-color: rgb(45, 78, 226);*/
                 }
-            /*
+            @else
                 header {
                     position: fixed;
                     top: -30px; /* Ajusta para que no interfiera con el margen de la página */
-                    /*left: 0;
+                    left: 0;
                     right: 0;
                     height: auto; /* Permite que el header crezca dinámicamente */
-                    /*text-align: center;
+                    text-align: center;
                     /*background-color:rgb(226, 45, 45); /* Fondo para que sea visible */
-                    /*font-family: 'arial', sans-serif;
-                /*}
+                    font-family: 'arial', sans-serif;
+                }
 
                 footer {
                     position: fixed;
                     bottom: -30px; /* Ajusta la posición */
-                    /*left: 0;
+                    left: 0;
                     right: 0;
                     height: auto;
                     text-align: center;
                     /*background-color: rgb(7, 231, 18)/* Fondo para que sea visible */
-                    /*font-family: 'arial', sans-serif;
+                    font-family: 'arial', sans-serif;
                 }
 
                 body {
-                    /*margin-top: 320px; /* Ajusta para que el contenido no se sobreponga al header */
-                    /*margin: 0;
-                    padding-top: 550px; /* Altura del header 435 antes */
-                    /*padding-bottom: 95px; /* Altura del footer */
-                    /*font-family: 'arial', sans-serif;
+                    margin-top: 550px; /* Ajusta para que el contenido no se sobreponga al header */
+                    /*margin: 0;*/
+                    padding-top: 0px; /* Altura del header */
+                    padding-bottom: 0px; /* Altura del footer */
+                    font-family: 'arial', sans-serif;
                     /*background-color:rgb(45, 78, 226); /* Fondo para que sea visible */
-                /*}
+                }
+                @endif
 
-                */
                 .datosgenerales{
                     border: 0px !important;
                     text-align: center;
@@ -772,10 +773,10 @@
                                     </th>
                                 </tr>
 
-                                {{-- 🔹 SALTO DE PÁGINA POR BLOQUE 
-                                <tr style="page-break-after: always;">
+                                {{-- 🔹 SALTO DE PÁGINA POR BLOQUE --}}
+                                <tr style="page-break-after: always;" class="sinBordetd">
                                     <td colspan="14"></td>
-                                </tr>--}}
+                                </tr>
 
                             @endforeach
 
