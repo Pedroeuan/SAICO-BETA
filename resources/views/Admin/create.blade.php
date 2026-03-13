9416357975abe0343cd9824d4ff755050ecdda4e
Pedroeuan <pedro.euan@aicosc.com>
2026-03-13 10:38:50 -0600

    UPDATE-SGI
    SAICO | Se agrega rol de SGI

--- resources/views/Admin/create.blade.php
+++ resources/views/Admin/create.blade.php
@@ -83,6 +83,7 @@
                                     <option value="Equipos" {{ old('RolUsuario') == 'Equipos' ? 'selected' : '' }}>Equipos</option>
                                     <option value="Laboratorio" {{ old('RolUsuario') == 'Laboratorio' ? 'selected' : '' }}>Laboratorio</option>
                                     <option value="Tics" {{ old('RolUsuario') == 'Tics' ? 'selected' : '' }}>Tics</option>
+                                    <option value="SGI" {{ old('RolUsuario') == 'SGI' ? 'selected' : '' }}>SGI</option>
                                 @else
                                     <option value="Super Administrador" {{ old('RolUsuario') == 'Super Administrador' ? 'selected' : '' }}>Super Administrador</option>
                                     <option value="Administrador" {{ old('RolUsuario') == 'Administrador' ? 'selected' : '' }}>Administrador</option>
@@ -93,6 +94,7 @@
                                     <option value="Equipos" {{ old('RolUsuario') == 'Equipos' ? 'selected' : '' }}>Equipos</option>
                                     <option value="Laboratorio" {{ old('RolUsuario') == 'Laboratorio' ? 'selected' : '' }}>Laboratorio</option>
                                     <option value="Tics" {{ old('RolUsuario') == 'Tics' ? 'selected' : '' }}>Tics</option>
+                                    <option value="SGI" {{ old('RolUsuario') == 'SGI' ? 'selected' : '' }}>SGI</option>
                                 @endif
                             </select>
                             @error('RolUsuario')
