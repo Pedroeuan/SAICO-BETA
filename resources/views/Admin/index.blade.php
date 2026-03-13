df838b29c3b408a8cae166391139454561140533
Pedroeuan <pedro.euan@aicosc.com>
2026-03-13 10:53:49 -0600

    update

--- resources/views/Admin/index.blade.php
+++ resources/views/Admin/index.blade.php
@@ -50,7 +50,7 @@
                         <td>{{ $Usuario->name }}</td>
                         <td>{{ $Usuario->email}}</td>
                         <td>{{ $Usuario->rol }}</td>
-                        <td>{{ $Usuario->formatted_date }}</td>
+                        <td>@if($Usuario->formatted_date == '01-01-2001') SIN FECHA ASIGNADA @else {{ $Usuario->formatted_date }} @endif</td>
                         <td>{{ $Usuario->Estatus }}</td>
                         @if($rol == 'Administrador' && $Usuario->rol == 'Super Administrador')
                             <td>
