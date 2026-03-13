faa2b4030a940bf44d6cbbc75089cd94e683e86e
Pedroeuan <pedro.euan@aicosc.com>
2026-03-12 18:31:25 -0600

    update

--- resources/views/Reportes/ReportesFotosPDF/Reporte_FOTOS_FOR_01_INS_16_PDF.blade.php
+++ resources/views/Reportes/ReportesFotosPDF/Reporte_FOTOS_FOR_01_INS_16_PDF.blade.php
@@ -285,13 +285,13 @@
                             <th>LUGAR:</th>
                             <td class="lineaInferior">{{ $Detalles_Generales['Lugar'] }}</td>
                             <th>HORA DE INSPECCIÓN:</th>
-                            <td class="lineaInferior">{{ $Detalles_Generales['Isometrico_Plano'] }}</td>
+                            <td class="lineaInferior"></td>
                         </tr>
                         <tr>
                             <th >PROCEDIMIENTO:</th>
                             <td class="lineaInferior">{{ $Detalles_Generales['Procedimiento'] }}</td>
                             <th style="width: 160px;">CRITERIO DE EVALUACIÓN:</th>
-                            <td class="lineaInferior">{{ $Detalles_Generales['Criterio_Evaluacion'] }}</td>
+                            <td class="lineaInferior"></td>
                         </tr>
                     </tbody>
                 </table>
