9febc265914bc982d5fd1dc7c98732b3f762fc2a
Pedroeuan <pedro.euan@aicosc.com>
2026-03-20 18:43:24 -0600

    up

--- app/Http/Controllers/Reporte/INS/FOR_02_PRO_INS_02Controller.php
+++ app/Http/Controllers/Reporte/INS/FOR_02_PRO_INS_02Controller.php
@@ -340,11 +340,8 @@ class FOR_02_PRO_INS_02Controller extends Controller
             'Datos_Equipo.TEMPERATURA_PRUEBA' => 'nullable|string',
             'Datos_Equipo.Observaciones' => 'nullable|string',
 
-            /*Titulos Juntas */
-            //'titulos' => 'nullable|array',  // Asegura que sea un array
-            //'titulos.*' => 'string',  // Cada título debe ser un string válido
-    
             /* Resultados Juntas */
+            'titulos_data' => 'nullable|string', // JSON con [{id,text},...]
             'titulos_data' => 'nullable|string', // JSON con [{id,text},...]
             'componente' => 'nullable|array',
             'no_ind' => 'nullable|array',
@@ -355,7 +352,11 @@ class FOR_02_PRO_INS_02Controller extends Controller
             'ht' => 'nullable|array',
             'evaluacion' => 'nullable|array',
             'longitud_inspeccionada' => 'nullable|array',
-    
+
+            'Long_Inspecc' => 'nullable|array',
+            'Long_Inspecc.*' => 'nullable|array',
+            'Long_Inspecc.*.*' => 'nullable|string|max:255',
+            
             //Validar el campo NumFirmas
             'numFirmas' => 'nullable|integer|in:1,2,3,4',
 
