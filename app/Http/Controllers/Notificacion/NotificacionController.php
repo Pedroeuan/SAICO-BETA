37913d91266e56087f19e20d36748ea97cbaa243
Pedroeuan <pedro.euan@aicosc.com>
2026-06-20 11:41:14 -0600

    update-notificacion

--- app/Http/Controllers/Notificacion/NotificacionController.php
+++ app/Http/Controllers/Notificacion/NotificacionController.php
@@ -191,7 +191,7 @@ class NotificacionController extends Controller
                             $notificacion->save();
                             Log::info('Enviando correo a: ' . $usuario->email);
                             //📧 Enviar correo
-                            //$usuario->notify(new NotificacionCertificadoMailable($mensajeCorto, $mensajeLargoemail,$url));
+                            $usuario->notify(new NotificacionCertificadoMailable($mensajeCorto, $mensajeLargoemail,$url));
                         }
                     }
                 }
@@ -228,7 +228,7 @@ class NotificacionController extends Controller
                             $notificacion->save();
                             //📧 Enviar correo
                             Log::info('Enviando correo a: ' . $usuario->email);
-                            //$usuario->notify(new NotificacionCertificadoMailable($mensajeCorto, $mensajeLargoemail,$url));
+                            $usuario->notify(new NotificacionCertificadoMailable($mensajeCorto, $mensajeLargoemail,$url));
                         }
                     }
                 }
@@ -265,6 +265,229 @@ class NotificacionController extends Controller
                             $notificacion->save();
                             //📧 Enviar correo
                             Log::info('Enviando correo a: ' . $usuario->email);
+                            $usuario->notify(new NotificacionCertificadoMailable($mensajeCorto, $mensajeLargoemail,$url));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    public function crearNotificacionesCertificadosInterno()
+    {
+        // Obtener fechas límite para las consultas
+        $fechaActual = Carbon::now();
+        $fecha45DiasAntes = $fechaActual->copy()->addDays(45)->toDateString();
+        $fecha40DiasAntes = $fechaActual->copy()->addDays(40)->toDateString();
+        $fecha35DiasAntes = $fechaActual->copy()->addDays(35)->toDateString();
+        $fecha30DiasAntes = $fechaActual->copy()->addDays(30)->toDateString();
+        $fecha25DiasAntes = $fechaActual->copy()->addDays(25)->toDateString();
+        $fecha20DiasAntes = $fechaActual->copy()->addDays(20)->toDateString();
+        $fecha15DiasAntes = $fechaActual->copy()->addDays(15)->toDateString();
+        $fecha10DiasAntes = $fechaActual->copy()->addDays(10)->toDateString();
+        $fecha7DiasAntes = $fechaActual->copy()->addDays(7)->toDateString();
+        $fecha5DiasAntes = $fechaActual->copy()->addDays(5)->toDateString();
+        $fecha0DiasAntes = $fechaActual->copy()->addDays(0)->toDateString();
+
+        // Obtener todos los certificados que están relacionados con la tabla general_eyc
+        $certificados = Certificados::with('generaleyc.ISO') // Cargar la relación con general_eyc
+            ->whereIn('Prox_fecha_calibracion', [$fecha45DiasAntes,$fecha40DiasAntes,$fecha35DiasAntes, $fecha30DiasAntes, $fecha25DiasAntes,$fecha20DiasAntes, $fecha15DiasAntes, $fecha10DiasAntes, $fecha7DiasAntes, $fecha5DiasAntes, $fecha0DiasAntes])
+            ->orWhereIn('Fecha_calibracion', [$fecha45DiasAntes,$fecha40DiasAntes,$fecha35DiasAntes, $fecha30DiasAntes, $fecha25DiasAntes,$fecha20DiasAntes, $fecha15DiasAntes, $fecha10DiasAntes, $fecha7DiasAntes, $fecha5DiasAntes, $fecha0DiasAntes])
+            ->orWhereIn('Fecha_mantenimiento', [$fecha45DiasAntes,$fecha40DiasAntes,$fecha35DiasAntes, $fecha30DiasAntes, $fecha25DiasAntes,$fecha20DiasAntes, $fecha15DiasAntes, $fecha10DiasAntes, $fecha7DiasAntes, $fecha5DiasAntes, $fecha0DiasAntes])
+            ->orWhereIn('Prox_fecha_mantenimiento', [$fecha45DiasAntes,$fecha40DiasAntes,$fecha35DiasAntes, $fecha30DiasAntes, $fecha25DiasAntes,$fecha20DiasAntes, $fecha15DiasAntes, $fecha10DiasAntes, $fecha7DiasAntes, $fecha5DiasAntes, $fecha0DiasAntes])
+            ->orWhereIn('Fecha_Verificacion', [$fecha45DiasAntes,$fecha40DiasAntes,$fecha35DiasAntes, $fecha30DiasAntes, $fecha25DiasAntes,$fecha20DiasAntes, $fecha15DiasAntes, $fecha10DiasAntes, $fecha7DiasAntes, $fecha5DiasAntes, $fecha0DiasAntes])
+            ->orWhereIn('Prox_fecha_verificacion', [$fecha45DiasAntes,$fecha40DiasAntes,$fecha35DiasAntes, $fecha30DiasAntes, $fecha25DiasAntes,$fecha20DiasAntes, $fecha15DiasAntes, $fecha10DiasAntes, $fecha7DiasAntes, $fecha5DiasAntes, $fecha0DiasAntes])
+            ->get();
+
+        // Recorrer cada certificado
+        foreach ($certificados as $certificado) {
+            // Obtener el registro de general_eyc relacionado con el certificado
+            $generalEyc = $certificado->generalEyc;
+            $No_economico = $generalEyc->No_economico;
+            $Nombre_C = $generalEyc->Nombre_E_P_BP;
+            $url = url('edicion/editEyC/' . $certificado->idGeneral_EyC);
+            // Obtener el ISO relacionado
+            $iso = $generalEyc->ISO ? $generalEyc->ISO->NombreISO : null;
+
+            // Determinar el tipo de general_eyc
+            if ($generalEyc) {
+                $tipo = $generalEyc->Tipo;
+
+                // Según el tipo, definir qué fecha usar
+                if ($iso == '9001')
+                {
+                    if ($tipo === 'EQUIPOS') {
+                        $fechaCalibracion = $certificado->Prox_fecha_calibracion;
+                        $fechaMantenimiento = $certificado->Prox_fecha_mantenimiento;
+                        $fechaVerificacion = $certificado->Prox_fecha_verificacion;
+                    } elseif ($tipo === 'CONSUMIBLES' || $tipo === 'BLOCK Y PROBETA') {
+                        $fechaCalibracion = $certificado->Fecha_calibracion;
+                    } else {
+                        // Si no corresponde a ninguno de los tipos, continuar con el siguiente
+                        continue;
+                    }
+                }
+                else //if($iso == '17025')
+                {
+                    if ($tipo === 'EQUIPOS' || $tipo === 'BLOCK Y PROBETA') {
+                        $fechaCalibracion = $certificado->Prox_fecha_calibracion;
+                        $fechaMantenimiento = $certificado->Prox_fecha_mantenimiento;
+                        $fechaVerificacion = $certificado->Prox_fecha_verificacion;
+
+                    } elseif ($tipo === 'CONSUMIBLES') {
+                        $fechaCalibracion = $certificado->Fecha_calibracion;
+                    } else {
+                        // Si no corresponde a ninguno de los tipos, continuar con el siguiente
+                        continue;
+                    }
+                }
+
+                    // Convertir la fecha al formato DD-MM-YYYY
+                    $fechaCalibracionFormateada = Carbon::parse($fechaCalibracion)->format('d-m-Y');
+                    $fechaMantenimientoFormateada = Carbon::parse($fechaMantenimiento)->format('d-m-Y');
+                    $fechaVerificacionFormateada = Carbon::parse($fechaVerificacion)->format('d-m-Y');
+                    
+                    // Determinar los días restantes para la calibración
+                    $diasRestantesC = Carbon::now()->startOfDay()->diffInDays(Carbon::parse($fechaCalibracion)->startOfDay(),false);
+                    $diasRestantesM = Carbon::now()->startOfDay()->diffInDays(Carbon::parse($fechaMantenimiento)->startOfDay(),false);
+                    $diasRestantesV = Carbon::now()->startOfDay()->diffInDays(Carbon::parse($fechaVerificacion)->startOfDay(),false);
+
+                // Filtrar usuarios según el ISO
+                $usuarios = User::where('Estatus', 'ALTA')
+                    ->where(function($query) use ($iso) {
+                        $query->whereIn('rol', ['Super Administrador', 'Administrador']);
+                        if ($iso == '17025') {
+                            $query->orWhere('rol', 'Laboratorio');
+                        }
+                        if ($iso == '9001') {
+                            $query->orWhere('rol', 'Equipos');
+                        }
+                    })
+                    ->get();
+
+                // ===== NOTIFICACIÓN DE CALIBRACIÓN =====
+                if ($diasRestantesC >= 0 && $diasRestantesC <= 45) {
+                    if ($diasRestantesC == 0) {
+                        if ($tipo === 'EQUIPOS') {
+                            $mensajeCorto = "Calibración VENCIDA";
+                            $mensajeLargo = "La Calibración del Equipo: ".$Nombre_C.", Con No. economico: " . $No_economico . " esta VENCIDA (Fecha de vencimiento: " . $fechaCalibracionFormateada . ")";
+                            $mensajeLargoemail = "La Calibración del Equipo: ".$Nombre_C.", <br>Con No. economico: " . $No_economico . "<br>esta <span style='color: #E01A22;'>VENCIDA</span><br>(Fecha de vencimiento: <span style='color: #E01A22;'>" . $fechaCalibracionFormateada . "</span>)";
+                        } elseif ($tipo === 'CONSUMIBLES') {
+                            $mensajeCorto = "Certificado CADUCADO";
+                            $mensajeLargo = "El Certificado del Consumible: ".$Nombre_C.", Con el No. certificado: " . $certificado->No_certificado . " está CADUCADO (Fecha de vencimiento: " . $fechaCalibracionFormateada . ")";
+                            $mensajeLargoemail = "El Certificado del Consumible: ".$Nombre_C.", <br>Con el No. certificado: " . $certificado->No_certificado . "<br>está <span style='color: #E01A22;'>CADUCADO </span><br>(Fecha de vencimiento: <span style='color: #E01A22;'>" . $fechaCalibracionFormateada . "</span>)";
+                        } elseif ($tipo === 'BLOCK Y PROBETA') {
+                            $mensajeCorto = "Calibración VENCIDA";
+                            $mensajeLargo = "El Block y Probeta: ".$Nombre_C.", La Calibración del No. economico: " . $No_economico . " esta VENCIDA (Fecha de vencimiento: " . $fechaCalibracionFormateada . ")";
+                            $mensajeLargoemail = "El Block y Probeta: ".$Nombre_C.", <br>La Calibración del No. economico: " . $No_economico . "<br>esta <span style='color: #E01A22;'> VENCIDA </span><br>(Fecha de vencimiento: <span style='color: #E01A22;'>" . $fechaCalibracionFormateada . "</span>)";
+                        }
+                    } else {
+                        if ($tipo === 'EQUIPOS') {
+                            $mensajeCorto = "Calib. Prox. a VENCER en $diasRestantesC días";
+                            $mensajeLargo = "La Calibración del Equipo: ".$Nombre_C.", Con No. economico: " . $No_economico . " está próximo a VENCER en $diasRestantesC días (Fecha de vencimiento: " . $fechaCalibracionFormateada . ")";
+                            $mensajeLargoemail = "La Calibración del Equipo: ".$Nombre_C.", <br>Con No. economico: " . $No_economico . " <br>está próximo a <span style='color: #E01A22;'>VENCER en $diasRestantesC días</span><br>(Fecha de vencimiento: <span style='color: #E01A22;'>" . $fechaCalibracionFormateada . "</span>)";
+                        } elseif ($tipo === 'CONSUMIBLES') {
+                            $mensajeCorto = "Cert. Prox. a CADUCAR en $diasRestantesC días";
+                            $mensajeLargo = "El Certificado del Consumible: ".$Nombre_C.", Con No. certificado: " . $certificado->No_certificado . " está próximo a CADUCAR en $diasRestantesC días (Fecha de vencimiento: " . $fechaCalibracionFormateada . ")";
+                            $mensajeLargoemail = "El Certificado del Consumible: ".$Nombre_C.", <br>Con No. certificado: " . $certificado->No_certificado . " <br>está próximo a <span style='color: #E01A22;'> CADUCAR en $diasRestantesC días</span> <br>(Fecha de vencimiento: " . $fechaCalibracionFormateada . "</span>)";
+                        } elseif ($tipo === 'BLOCK Y PROBETA') {
+                            $mensajeCorto = "Calib. Prox. a VENCER en $diasRestantesC días";
+                            $mensajeLargo = "La Calibración del Block y Probeta: ".$Nombre_C.", Con el No. economico: " . $No_economico . " está próximo a VENCER en $diasRestantesC días (Fecha de vencimiento: " . $fechaCalibracionFormateada . ")";
+                            $mensajeLargoemail = "La Calibración del Block y Probeta: ".$Nombre_C.", <br>Con el No. economico: " . $No_economico . " <br>está próximo a <span style='color: #E01A22;'> VENCER en $diasRestantesC días</span> <br>(Fecha de vencimiento: " . $fechaCalibracionFormateada . "</span>)";
+                        }
+                    }
+
+                    foreach ($usuarios as $usuario) {
+                        $notificacionExistente = Notificacion::where('users_id', $usuario->id)
+                            ->where('Mensaje_Corto', $mensajeCorto)
+                            ->where('Mensaje_Largo', $mensajeLargo)
+                            ->first();
+
+                        if (!$notificacionExistente) {
+                            $notificacion = new Notificacion();
+                            $notificacion->users_id = $usuario->id;
+                            $notificacion->Mensaje_Corto = $mensajeCorto;
+                            $notificacion->Mensaje_Largo = $mensajeLargo;
+                            $notificacion->url = $url;
+                            $notificacion->leida = false;
+                            $notificacion->save();
+                            //Log::info('Enviando correo a: ' . $usuario->email);
+                            //📧 Enviar correo
+                            //$usuario->notify(new NotificacionCertificadoMailable($mensajeCorto, $mensajeLargoemail,$url));
+                        }
+                    }
+                }
+
+                // ===== NOTIFICACIÓN DE MANTENIMIENTO =====
+                if ($diasRestantesM >= 0 && $diasRestantesM <= 45) {
+                    if ($diasRestantesM == 0) {
+                        if ($tipo === 'EQUIPOS') {
+                            $mensajeCorto = "Mantenimiento VENCIDO";
+                            $mensajeLargo = "El Mantenimiento del Equipo: ".$Nombre_C.", Con No. economico: " . $No_economico . " esta VENCIDO (Fecha de vencimiento: " . $fechaMantenimientoFormateada . ")";
+                            $mensajeLargoemail = "El Mantenimiento del Equipo: ".$Nombre_C.", <br>Con No. economico: " . $No_economico . "<br>esta <span style='color: #E01A22;'>VENCIDO</span><br>(Fecha de vencimiento: <span style='color: #E01A22;'>" . $fechaMantenimientoFormateada . "</span>)";
+                        }
+                    } else {
+                        if ($tipo === 'EQUIPOS') {
+                            $mensajeCorto = "Mant. Prox. a VENCER en $diasRestantesM días";
+                            $mensajeLargo = "El Mantenimiento del Equipo: ".$Nombre_C.", Con No. economico: " . $No_economico . " está próximo a VENCER en $diasRestantesM días (Fecha de vencimiento: " . $fechaMantenimientoFormateada . ")";
+                            $mensajeLargoemail = "El Mantenimiento del Equipo: ".$Nombre_C.", <br>Con No. economico: " . $No_economico . " <br>está próximo a <span style='color: #E01A22;'>VENCER en $diasRestantesM días</span><br>(Fecha de vencimiento: <span style='color: #E01A22;'>" . $fechaMantenimientoFormateada . "</span>)";
+                        }
+                    }
+
+                    foreach ($usuarios as $usuario) {
+                        $notificacionExistente = Notificacion::where('users_id', $usuario->id)
+                            ->where('Mensaje_Corto', $mensajeCorto)
+                            ->where('Mensaje_Largo', $mensajeLargo)
+                            ->first();
+
+                        if (!$notificacionExistente) {
+                            $notificacion = new Notificacion();
+                            $notificacion->users_id = $usuario->id;
+                            $notificacion->Mensaje_Corto = $mensajeCorto;
+                            $notificacion->Mensaje_Largo = $mensajeLargo;
+                            $notificacion->url = $url;
+                            $notificacion->leida = false;
+                            $notificacion->save();
+                            //📧 Enviar correo
+                            //Log::info('Enviando correo a: ' . $usuario->email);
+                            //$usuario->notify(new NotificacionCertificadoMailable($mensajeCorto, $mensajeLargoemail,$url));
+                        }
+                    }
+                }
+
+                // ===== NOTIFICACIÓN DE VERIFICACIÓN =====
+                if ($diasRestantesV >= 0 && $diasRestantesV <= 45) {
+                    if ($diasRestantesV == 0) {
+                        if ($tipo === 'EQUIPOS') {
+                            $mensajeCorto = "Verificación VENCIDA";
+                            $mensajeLargo = "La Verificación del Equipo: ".$Nombre_C.", Con No. economico: " . $No_economico . " esta VENCIDA (Fecha de vencimiento: " . $fechaVerificacionFormateada . ")";
+                            $mensajeLargoemail = "La Verificación del Equipo: ".$Nombre_C.", <br>Con No. economico: " . $No_economico . "<br>esta <span style='color: #E01A22;'>VENCIDA</span><br>(Fecha de vencimiento: <span style='color: #E01A22;'>" . $fechaVerificacionFormateada . "</span>)";
+                        }
+                    } else {
+                        if ($tipo === 'EQUIPOS') {
+                            $mensajeCorto = "Verif. Prox. a VENCER en $diasRestantesV días";
+                            $mensajeLargo = "La Verificación del Equipo: ".$Nombre_C.", Con No. economico: " . $No_economico . " está próximo a VENCER en $diasRestantesV días (Fecha de vencimiento: " . $fechaVerificacionFormateada . ")";
+                            $mensajeLargoemail = "La Verificación del Equipo: ".$Nombre_C.", <br>Con No. economico: " . $No_economico . " <br>está próximo a <span style='color: #E01A22;'>VENCER en $diasRestantesV días</span><br>(Fecha de vencimiento: <span style='color: #E01A22;'>" . $fechaVerificacionFormateada . "</span>)";
+                        }
+                    }
+
+                    foreach ($usuarios as $usuario) {
+                        $notificacionExistente = Notificacion::where('users_id', $usuario->id)
+                            ->where('Mensaje_Corto', $mensajeCorto)
+                            ->where('Mensaje_Largo', $mensajeLargo)
+                            ->first();
+
+                        if (!$notificacionExistente) {
+                            $notificacion = new Notificacion();
+                            $notificacion->users_id = $usuario->id;
+                            $notificacion->Mensaje_Corto = $mensajeCorto;
+                            $notificacion->Mensaje_Largo = $mensajeLargo;
+                            $notificacion->url = $url;
+                            $notificacion->leida = false;
+                            $notificacion->save();
+                            //📧 Enviar correo
+                            //Log::info('Enviando correo a: ' . $usuario->email);
                             //$usuario->notify(new NotificacionCertificadoMailable($mensajeCorto, $mensajeLargoemail,$url));
                         }
                     }
